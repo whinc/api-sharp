93840ce9fbc0b0483b1e48633d0585015a0fa86c
whincwu <whincwu@163.com>
2019-09-29 11:52:22 +0800

    fix: 修复控制台错误
    未导入prop-types时会打印错误，改成告警

--- src/PropTypes.ts
+++ src/PropTypes.ts
@@ -2,7 +2,9 @@ let PropTypes
 try {
   PropTypes = require("prop-types")
 } catch (err) {
-  console.error(err)
+  console.warn(
+    `"prop-types" is an optional dependency and is not included by default. If you need to use runtime type checking, you need to import the "prop-types" package before importing "api-sharp".`
+  )
 }
 
 const checkPropTypes = PropTypes ? PropTypes.checkPropTypes : () => {}
