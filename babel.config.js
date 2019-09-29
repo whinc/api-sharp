c4867f9c1ba5eff9af718153c734ed373bbcc711
whincwu <whincwu@163.com>
2019-09-29 11:38:15 +0800

    fix: 修复babel配置错误

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// 这里的配置同时给 tsdx 和 jest 使用
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { browsers: "defaults", node: 8 }, useBuiltIns: "usage", corejs: 3 }]]
+  presets: [
+    ["@babel/preset-env", { targets: { chrome: "48", ie: "11", node: 8 }, useBuiltIns: "usage", corejs: 3 }],
+    "@babel/preset-typescript"
+  ],
+  plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]]
 }
